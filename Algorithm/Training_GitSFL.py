32c53b6052ccc1cec957a8908179515ca8bd54df
Zeke Xia <13913226174@163.com>
2023-10-12 20:09:08 +0800

    update

--- Algorithm/Training_GitSFL.py
+++ Algorithm/Training_GitSFL.py
@@ -1,24 +1,37 @@
+import copy
+import random
 from typing import List
 
 import numpy as np
-import torch
+
 from loguru import logger
 
 from Algorithm.Training_ASync import Training_ASync
+from models import Aggregation
 from utils.utils import getTrueLabels
+
+AMOUNT_OF_HELPERS = 3
+AMOUNT_OF_ACTIVATION = 50
 
 
 @logger.catch
 class GitSFL(Training_ASync):
-    def __init__(self, args, net_glob, dataset_train, dataset_test, dict_users):
+    def __init__(self, args, net_glob, dataset_train, dataset_test, dict_users, net_glob_client, net_glob_server):
         super().__init__(args, net_glob, dataset_train, dataset_test, dict_users)
 
         # GitSFL Setting
         self.repoSize = int(args.num_users * args.frac)
         self.modelServer = []
         self.modelClient = []
+        self.modelVersion = [0 for _ in range(self.repoSize)]
         self.true_labels = getTrueLabels(self)
         self.selected_count = [0 for _ in range(args.num_users)]
+
+        self.net_glob_client = net_glob_client
+        self.net_glob_server = net_glob_server
+
+        self.distance = self.countDistance()
+        self.dataByLabel = self.organizeDataByLabel()
 
     @logger.catch()
     def train(self):
@@ -32,16 +45,17 @@ class GitSFL(Training_ASync):
 
         while self.time < self.args.limit_time:
             client_index, modelIndex, model_version, trainTime = self.update_queue.pop(0)
+            self.modelVersion[modelIndex] += 1
             for update in self.update_queue:
                 update[-1] -= trainTime
 
-            self.splitTrain()
+            self.splitTrain(client_index)
 
             self.Agg()
 
             self.test()
 
-            self.weakAgg()
+            self.weakAgg(modelIndex)
 
             nextClient = self.selectNextClient()
             self.update_queue.append([nextClient, modelIndex, model_version + 1, self.clients.getTime(nextClient)])
@@ -50,18 +64,76 @@ class GitSFL(Training_ASync):
             self.idle_clients.remove(nextClient)
             self.idle_clients.add(client_index)
 
-    def splitTrain(self):
-        sampledActivation = self.sampleActivation()
+    def splitTrain(self, curClient:int):
+        helpers = self.selectHelpers(curClient)
+        sampledActivation = [self.sampleData(helper) for helper in helpers]
+
         pass
 
     def Agg(self):
-        pass
+        # Agg Client
+        w_client = [copy.deepcopy(model_client.state_dict) for model_client in self.modelClient]
+        w_server = [copy.deepcopy(model_server.state_dict) for model_server in self.modelClient]
+        w_avg_client = Aggregation(w_client, self.modelVersion)
+        w_avg_server = Aggregation(w_server, self.modelVersion)
+
+        self.net_glob_client.load_state_dict(w_avg_client)
+        self.net_glob_server.load_state_dict(w_avg_server)
+
+        self.net_glob.load_state_dict(w_avg_client)
+        self.net_glob.load_state_dict(w_avg_server)
 
     def selectNextClient(self) -> int:
-        pass
+        nextClient = random.choice(list(self.idle_clients))
+        return nextClient
 
-    def weakAgg(self):
-        pass
+    def weakAgg(self, modelIdx:int):
+        cur_model_client = self.modelClient[modelIdx]
+        cur_model_server = self.modelServer[modelIdx]
+        weight = max(10 + self.modelVersion[modelIdx] - np.mean(self.modelVersion), 2)
+        w = [copy.deepcopy(self.net_glob_client.state_dict()), copy.deepcopy(cur_model_client)]
+        lens = [1, weight]
+        w_avg_client = Aggregation(w, lens)
+        cur_model_client.load_state_dict(w_avg_client)
 
-    def sampleActivation(self) -> List[torch.tensor]:
-        pass
+        w = [copy.deepcopy(self.net_glob_server.state_dict()), copy.deepcopy(cur_model_server)]
+        w_avg_server = Aggregation(w, lens)
+        cur_model_client.load_state_dict(w_avg_server)
+
+    def sampleData(self, helper:int) -> List[int]:
+        # randomSample
+        sampledNum = [int(AMOUNT_OF_ACTIVATION * (num / sum(self.true_labels[helper]))) for num in
+                      self.true_labels[helper]]
+        sampledData = []
+        for classIdx, num in enumerate(sampledNum):
+            sampledData.extend(random.sample(self.dataByLabel[helper][classIdx], num))
+        return sampledData
+
+    def selectHelpers(self, curClient:int) -> List[int]:
+        curDistance = []
+        for i, distance in enumerate(self.distance[curClient]):
+            if i == curClient:
+                continue
+            curDistance.append((i, distance))
+
+        curDistance.sort(key=lambda x: x[-1])
+        helpers = [i[0] for i in curDistance[:AMOUNT_OF_HELPERS]]
+        return helpers
+
+    def countDistance(self) -> list[list[int]]:
+        distance = [[-1 for _ in range(self.args.num_users)] for _ in range(self.args.num_users)]
+        for i in range(self.args.num_users):
+            for j in range(i + 1, self.args.num_users):
+                dot = np.dot(self.true_labels[i], self.true_labels[j])
+                distance[i][j] = distance[j][i] = dot
+        return distance
+
+    def organizeDataByLabel(self) -> list[list[list[int]]]:
+        organized = []
+        for client in self.args.num_users:
+            res = [[] for _ in range(self.args.num_classes)]
+            all_local_data = self.dict_users[client]
+            for data in all_local_data:
+                res[self.dataset_train[data][1]].append(data)
+            organized.append(res)
+        return organized
---
+++ models/SplitModel.py
@@ -0,0 +1,120 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import math
+
+
+class ResBlk(nn.Module):
+    def __init__(self, ch_in, ch_out, stride):
+        super(ResBlk, self).__init__()
+        self.conv1 = nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(ch_out)
+        self.conv2 = nn.Conv2d(ch_out, ch_out, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(ch_out)
+
+        self.extra = nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=1, stride=stride),
+            nn.BatchNorm2d(ch_out)
+        )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out = self.extra(x) + out
+        out = F.relu(out)
+        return out
+
+
+# Model at client side
+class ResNet18_client_side(nn.Module):
+    def __init__(self, type):
+        super(ResNet18_client_side, self).__init__()
+        self.type = type
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=3, padding=0),
+            nn.BatchNorm2d(64)
+        )
+        self.layer2 = ResBlk(64, 64, stride=2)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = self.layer2(x)
+        return x
+
+
+# Model at server side
+class ResNet18_server_side(nn.Module):
+    def __init__(self):
+        super(ResNet18_server_side, self).__init__()
+        self.blk2 = ResBlk(64, 128, stride=2)
+        self.blk3 = ResBlk(128, 256, stride=2)
+        self.blk4 = ResBlk(256, 512, stride=2)
+        self.outlayer = nn.Linear(512 * 1 * 1, 10)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.blk2(x)
+        x = self.blk3(x)
+        x = self.blk4(x)
+        x = F.adaptive_avg_pool2d(x, [1, 1])
+        x = x.view(x.size(0), -1)
+        x = self.outlayer(x)
+
+        return x
+
+
+class VGG16_client_side(nn.Module):
+
+    def __init__(self):
+        super(VGG16_client_side, self).__init__()
+        self.layer1 = nn.Sequential(nn.Conv2d(3, 64, 3, 1, 1), nn.BatchNorm2d(64), nn.ReLU(),
+                                    nn.Conv2d(64, 64, 3, 1, 1), nn.BatchNorm2d(64), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.layer2 = nn.Sequential(nn.Conv2d(64, 128, 3, 1, 1), nn.BatchNorm2d(128), nn.ReLU(),
+                                    nn.Conv2d(128, 128, 3, 1, 1), nn.BatchNorm2d(128), nn.ReLU(), nn.MaxPool2d(2, 2))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        return x
+
+
+class VGG16_server_side(nn.Module):
+
+    def __init__(self):
+        super(VGG16_server_side, self).__init__()
+        self.layer3 = nn.Sequential(nn.Conv2d(128, 256, 3, 1, 1), nn.BatchNorm2d(256), nn.ReLU(),
+                                    nn.Conv2d(256, 256, 3, 1, 1), nn.BatchNorm2d(256), nn.ReLU(),
+                                    nn.Conv2d(256, 256, 3, 1, 1), nn.BatchNorm2d(256), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.layer4 = nn.Sequential(nn.Conv2d(256, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(),
+                                    nn.Conv2d(512, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(),
+                                    nn.Conv2d(512, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.layer5 = nn.Sequential(nn.Conv2d(512, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(),
+                                    nn.Conv2d(512, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(),
+                                    nn.Conv2d(512, 512, 3, 1, 1), nn.BatchNorm2d(512), nn.ReLU(), nn.MaxPool2d(2, 2))
+        self.fc1 = nn.Sequential(nn.Flatten(), nn.Linear(512, 512), nn.ReLU(), nn.Dropout())
+        self.fc2 = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Dropout())
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
